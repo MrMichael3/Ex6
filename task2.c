45e87ee8f55e315958220e85c831db5bcb3d6888
Michael <michael.kuechler2@uzh.ch>
2018-05-20 11:50:11 +0200

    clean version

--- task2.c
+++ task2.c
@@ -7,7 +7,6 @@ struct tuple{
 	int val;
 	int k;
 };
-//matrix buy x sell
 struct tuple m[60][60];
 
 //get the previous maximum
@@ -15,7 +14,6 @@ struct tuple maxPrev(int buy){
 	struct tuple max;
 	max.val = 0;
 	max.k = 0;
-	//try top-down to improve algorithm
 	for(int b=0;b<buy-1;b++){
 		for(int s=b+1;s<buy;s++){
 			if(m[b][s].val > max.val){
@@ -26,63 +24,33 @@ struct tuple maxPrev(int buy){
 	}
 	return max;
 }
-//solution without buy limitation (k)
-int maxProfitLight(int price[], int n, int k){
+int maxProfit(int price[], int n, int k){
 	int max = 0;
-	int isChanged = 0;
 	for(int b=0;b<n-1;b++){
 		for(int s=b+1;s<n;s++){
 			//add if greater than previous max and k limitation isnt reached
 			if(price[s]-price[b] + maxPrev(b).val > max && maxPrev(b).k + 1 <= k){
-				//isChanged = 1;
 				max = price[s]-price[b]+maxPrev(b).val;
 				m[b][s].val = max;
-				//when do I need to increase k
-				printf("kPrev= %d\n",maxPrev(b).k);
 				m[b][s].k = maxPrev(b).k + 1;
-				printf("max: %d,k: %d in [%d][%d]\n",max,m[b][s].k,b,s);
 			}
 		}
-		/*if(isChanged){
-			k--;
-			isChanged = 0;
-			if(k<= 0){
-				return max;
-			}
-		}*/
 	}
 	return max;
 }
-/*
-int maxProfit(int price[], int n, int k){
-	int max;
-	
-	for(int x=1;x<=k;x++){
-		for(int b=0;b<n-1;b++){
-			max = 0;
-			for(int s=b+1;s<n;s++){
-				if(price[s]-price[b]>max){
-					max = price[s]-price[b];
-					printf("max: %d in [%d][%d]\n",max,b,s);
-					m[b][s] = max;
-				}
-			}
-			
-		}
-	}
-	return maxPrev(n);
-}*/
 
 int main(){
-	int price[8] = {12,14,17,10,14,13,12,19};
-	int n = 8;
-	int k = 1;
-	int profit = maxProfitLight(price,n,k);
-	//profit = maxProfitLight(price,n);
-	printf("maxProfit: %d\n",profit);
+	//Example from task sheet
 	
+	int price1[8] = {12,14,17,10,14,13,12,15};
+	int n1 = 8;
+	int k1 = 3;
+	int profit1 = maxProfit(price1,n1,k1);
+	printf("maxProfit: %d\n",profit1);
+
+	//User input
 	
-	/*int price[50];
+	int price[50];
 	int k;
 	int n=0;
 	printf("How much transactions (k)?\n");
@@ -92,6 +60,7 @@ int main(){
 		n++;
 	}
 	int profit = maxProfit(price,n,k);
-	printf("maxProfit: %d\n",profit);*/
+	printf("maxProfit: %d\n",profit);
+	
 	return 0;
 }
